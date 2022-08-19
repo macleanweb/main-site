9148e8f8bfea71c7b5ef73775e4ef2cec4e818f6
Andrew <90115111+macleanweb@users.noreply.github.com>
2022-08-19 10:53:25 +0200

    feat: add scss breakpoint mixins

--- next.config.js
+++ next.config.js
@@ -1,12 +1,14 @@
-/** @type {import('next').NextConfig} */
-const path = require('path');
+const path = require('path')
 
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
   sassOptions: {
     includePaths: [path.join(__dirname, 'styles')],
-    prependData: `@import '_variables.scss';`
+    prependData: `
+      @import '_variables.scss';
+      @import 'mixins/mixins.scss';
+      `,
   },
 }
 

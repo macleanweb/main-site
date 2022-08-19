9e746c9eccfd57dba4130a43347116568fbfd8c0
Andrew <90115111+macleanweb@users.noreply.github.com>
2022-08-19 12:08:05 +0200

    chore: remove deprecated code

--- utils/contentfulUtils.js
+++ utils/contentfulUtils.js
@@ -1,14 +1,6 @@
-const contentful = require('contentful')
-
 const SPACE_ID = process.env.CONTENTFUL_SPACE_ID
 const ENVIRONMENT = process.env.CONTENTFUL_ENVIRONMENT
 const ACCESS_TOKEN = process.env.CONTENTFUL_ACCESS_TOKEN
-
-const client = contentful.createClient({
-  space: SPACE_ID,
-  environment: ENVIRONMENT,
-  accessToken: ACCESS_TOKEN,
-})
 
 export async function makeContentfulQuery(query) {
   const result = await fetch(
